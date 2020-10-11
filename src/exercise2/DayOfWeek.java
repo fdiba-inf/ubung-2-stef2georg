a00ad27e1140b1e92552bb7344d34cfd2ae5adbe
Stefan Georgiev <stefan.georgiev91@gmail.com>
2020-10-11 22:20:06 +0300

    Update DayOfWeek.java

--- src/exercise2/DayOfWeek.java
+++ src/exercise2/DayOfWeek.java
@@ -9,33 +9,24 @@ public class DayOfWeek {
 
         System.out.print("Enter a number: ");
         int dayOfWeekNumber = input.nextInt();
-
+        
         String dayOfWeek;
-        switch (dayOfWeekNumber) {
-            case 1:
-                dayOfWeek = "Monday";
-                break;
-            case 2:
-                dayOfWeek = "Tuesday";
-                break;
-            case 3:
-                dayOfWeek = "Wednesday";
-                break;
-            case 4:
-                dayOfWeek = "Thursday";
-                break;
-            case 5:
-                dayOfWeek = "Friday";
-                break;
-            case 6:
-                dayOfWeek = "Saturday";
-                break;
-            case 7:
-                dayOfWeek = "Sunday";
-                break;
-            default:
-                dayOfWeek = "Invalid day";
-                break;
+        if (dayOfWeekNumber == 1) {
+            dayOfWeek = "Monday";
+        } else if (dayOfWeekNumber == 2) {
+            dayOfWeek = "Tuesday";
+        } else if (dayOfWeekNumber == 3) {
+            dayOfWeek = "Wednesday";
+        } else if (dayOfWeekNumber == 4) {
+            dayOfWeek = "Thursday";
+        } else if (dayOfWeekNumber == 5) {
+            dayOfWeek = "Friday";
+        } else if (dayOfWeekNumber == 6) {
+            dayOfWeek = "Saturday";
+        } else if (dayOfWeekNumber == 7) {
+            dayOfWeek = "Sunday";
+        } else {
+            dayOfWeek = "Invalid day";
         }
 
         System.out.println("Day of week: " + dayOfWeek);
